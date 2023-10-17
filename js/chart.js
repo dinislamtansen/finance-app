f8eda53e0cfcecb554149b6aa729405e4845f39e
tansen2425 <dinislamtansen11@gmail.com>
2023-10-17 15:19:03 +0600

    display done

---
+++ js/chart.js
@@ -0,0 +1,20 @@
+const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ['deposit', 'Withdraw', 'Total'],
+      datasets: [{
+        label: '# Transaction',
+        data: [12, 19, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
